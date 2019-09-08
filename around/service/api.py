8b9605226d7a59c4bd44e3865eae52476add336b
Ashok0111 <ashok8182@gmail.com>
2019-09-08 22:42:19 +0530

    added with new database

--- around/service/api.py
+++ around/service/api.py
@@ -4,28 +4,30 @@ from flask import Flask,request ,redirect, url_for
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 import json
-from mongoengine import *
+from flask_mongoengine import MongoEngine
+from mongoengine.connection import disconnect
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token, create_refresh_token,verify_jwt_in_request, jwt_required,jwt_optional, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt,get_jwt_claims)
 from passlib.hash import pbkdf2_sha256 as sha256
 app = Flask(__name__)
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JWT_SECRET_KEY'] = 'nevergiveup'
 app.config['JWT_ERROR_MESSAGE_KEY'] = 'status'  
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
 jwt = JWTManager(app)
 #CORS(app,resources={r"*": {"origins": "http://localhost:4200"}})
 CORS(app, resources={r"*": {"origins": "http://localhost:4200"}})
 
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
-    connect('around')
+    connect(alias='b4xab7lqny8ghgn')
     jti = decrypted_token['jti']
     token=TokenBlacklist()
     if token.validate_token(token=jti):
         return jsonify({'code': 401,'status': 'Invalid'})
-    
 
 @jwt.user_claims_loader
 def add_claims_to_access_token(identity):
@@ -60,7 +62,7 @@ def signup():
         return jsonify({'code': 400,'status': 'Password must be minimun 8 characters'})
     requestbody['password']= sha256.hash(requestbody['password'])   
     try:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user=User()
         is_valid=user.validate_record(requestbody['username'],requestbody['email'],requestbody['password'],requestbody['fname'],requestbody['lname'])
         if(is_valid == True):
@@ -71,10 +73,12 @@ def signup():
             return jsonify({'code': 200,'status': 'Success','access-token':access_token,'refresh-token':refresh_token})
         else:
             error = is_valid
-            return jsonify({'code': 400,'status': error}) 
+            return jsonify({'code': 400,'status': error})
+            disconnect(alias='b4xab7lqny8ghgn')
     except Exception as e:
         print(e)
         return jsonify({'code': 500,'status': 'Internal Server Error'})
+        disconnect(alias='b4xab7lqny8ghgn')
 
           
 @app.route('/validateusername',methods = ['POST'])
@@ -82,7 +86,7 @@ def signup():
 def validate_username():
     requestbody =json.loads(request.data)
     try:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user=User()
         is_valid = user.validate_username(requestbody['username'])
         if(is_valid == True):
@@ -97,7 +101,7 @@ def validate_username():
 def validate_email():
     requestbody =json.loads(request.data)
     try:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user=User()
         is_valid = user.validate_email(requestbody['email'])
         if(is_valid == True):
@@ -114,7 +118,7 @@ def validate_session():
     claims = get_jwt_claims()
     if claims:
         try:
-            connect('around')
+            connect(alias='b4xab7lqny8ghgn')
             user=User()
             is_valid = user.check_user_session(claims)
             if is_valid:
@@ -131,7 +135,7 @@ def validate_session():
 def signin():
     requestbody =json.loads(request.data)
     try:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user=User()
         is_valid = user.validate_sign_in(requestbody['email'],requestbody['password'])
         if is_valid:
@@ -148,7 +152,7 @@ def signin():
 @jwt_required
 @cross_origin()
 def signout():
-    connect('around')
+    connect(alias='b4xab7lqny8ghgn')
     jti = get_raw_jwt()['jti']
     blacklist =TokenBlacklist()
     blacklist.add_to_blacklist(jti)
@@ -159,7 +163,7 @@ def signout():
 def forgot_password():
     requestbody =json.loads(request.data)
     if requestbody:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user= User()
         if(user.forgot_password_otp(requestbody)):
             return jsonify({'code': 200,'status': 'Success'})
@@ -172,7 +176,7 @@ def auth_guard():
     requestbody =json.loads(request.data)
     print(requestbody,"requestbody")
     if requestbody:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         if(1==1):
             return jsonify({'code': 200,'status': 'Success'})
         return jsonify({'code': 400,'status': 'Something went wrong.'})
@@ -182,7 +186,7 @@ def auth_guard():
 def reset_password():
     requestbody =json.loads(request.data)
     if requestbody:
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         user= User()
         if(user.reset_password(requestbody)):
             return jsonify({'code': 200,'status': 'Success'})
@@ -198,7 +202,7 @@ def save_post():
     print(requestbody,"requestbody");
     try:
         claims = get_jwt_claims()
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         post=Post()
         is_valid = post.validate_post(requestbody,claims)
         if is_valid:
@@ -214,7 +218,7 @@ def save_post():
 def view_post(post_id):
     try:
         claims = get_jwt_claims()
-        connect('around')
+        connect(alias='b4xab7lqny8ghgn')
         post=Post()
         is_valid = post.view_post(post_id,claims)
         if is_valid:
@@ -233,7 +237,7 @@ def delete_post():
         post_id = request.args.get('post',False)
         if post_id:
             claims = get_jwt_claims()
-            connect('around')
+            connect(alias='b4xab7lqny8ghgn')
             post=Post()
             is_valid = post.delete_post(post_id,claims)
             if is_valid:
@@ -253,9 +257,9 @@ def search():
         object =  request.args.get('search',False)
         if search and object:
             claims = get_jwt_claims()
-            connect('around')
+            connect(alias='b4xab7lqny8ghgn')
             post=Post()
-            is_valid = post.search_around(request.args,claims)
+            is_valid = post.search_b4xab7lqny8ghgn(request.args,claims)
             if is_valid:
                 return jsonify({'code': 200,'status': 'Success','data':is_valid})
         return jsonify({'code': 400,'status': 'Something went wrong.'})
@@ -266,7 +270,9 @@ def search():
     
     
 if __name__ == '__main__':
+    db = MongoEngine(app)
     app.debug = True
     app.run(debug = True)
+    
     
     
